670a1960fd77670e0b697f498528f47bca2a43f3
dnt201 <nhatd2@vng.com.vn>
2024-05-18 13:30:01 +0700

    feat/Add module resolve to short import

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,39 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  // plugins: []
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@assets': './src/assets',
+          '@common': './src/common',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@layouts': './src/layouts',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@typings': './src/typings',
+          '@designs': './src/designs',
+          '@icons': './src/icons',
+          '@navigator': './src/navigator',
+          '@fragments': './src/fragments',
+          '@apiCaller': './src/apiCaller.ts',
+          '@themes': './src/themes',
+        },
+      },
+    ],
+  ],
 };
