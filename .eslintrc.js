17a863f6c5e5b65d484043a6940764428d85a16c
Nguyễn Văn Rin <Rinchan98.py@gmail.com>
2024-11-06 15:35:56 +0700

    add husky

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Use TypeScript parser
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    plugins: ['@typescript-eslint'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }, // Ignore vars starting with _
+      ],
+    },
+  };
